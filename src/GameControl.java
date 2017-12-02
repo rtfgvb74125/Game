0ae5cb34a8f7a320e5c8a2a20858ae8a2f147aad
rtfgvb74125 <105021056@live.asia.edu.tw>
2017-12-03 00:19:34 +0800

    morio_jump_countrol

--- src/GameControl.java
+++ src/GameControl.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -7,6 +9,7 @@ public class GameControl extends JFrame{
     private int ScreenW = Toolkit.getDefaultToolkit().getScreenSize().width;
     private int ScreenH = Toolkit.getDefaultToolkit().getScreenSize().height;
     private int frmW = 1000,frmH = 800,MarioX = 500,MarioY = 596;
+    private Timer t1 ;
     private ImageIcon mario = new ImageIcon("Image/m.png");
     private ImageIcon marioleft = new ImageIcon("Image/mleft.png");
     private ImageIcon marioup = new ImageIcon("Image/mup.png");
@@ -19,6 +22,8 @@ public class GameControl extends JFrame{
     private JTextField jtxTime = new JTextField("0");
     private JPanel jpnSouth = new JPanel(new GridLayout(1,7,3,3));
     private Container cp;
+    private boolean cheak = true;
+    private boolean cheak2 = false;
     public GameControl(){
         init();
     }
@@ -57,6 +62,7 @@ public class GameControl extends JFrame{
                         MarioX+=10;
                         jlabMario.setIcon(mario);
                         jlabMario.setLocation(MarioX,MarioY);
+
                         break;
                     case KeyEvent.VK_LEFT:
                         MarioX-=10;
@@ -64,11 +70,8 @@ public class GameControl extends JFrame{
                         jlabMario.setLocation(MarioX,MarioY);
                         break;
                     case KeyEvent.VK_UP:
-                        MarioY-=10;
-                        jlabMario.setIcon(marioup);
-//                        if(MarioY != 546){
-//
-//                        }
+                     t1.start();
+                     jlabMario.setIcon(marioup);
                 }
             }
 
@@ -80,6 +83,27 @@ public class GameControl extends JFrame{
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
 
+        t1 = new Timer(5, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (MarioY > 496 && cheak) {
+                    MarioY--;
+                    if (MarioY == 496) {
+                        cheak = false;
+                    }
+                    jlabMario.setLocation(MarioX, MarioY);
+                }else{
+                    MarioY++;
+                    if (MarioY==596) {
+                        cheak = true;
+                        t1.stop();
+                        jlabMario.setIcon(mario);
+                    }
+
+                    jlabMario.setLocation(MarioX, MarioY);
+                }
+            }
+        });
     }
 
 
