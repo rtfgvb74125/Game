61fb32f433452117fdbcbd819428b1556e18747e
rtfgvb74125 <105021056@live.asia.edu.tw>
2017-12-23 14:28:24 +0800

    20171223

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-//        GameControl gamec = new GameControl();
-//        gamec.setVisible(true);
+        GameControl gamec = new GameControl();
+        gamec.setVisible(true);
 //        LoginGame LoGame = new LoginGame();
 //        LoGame.setVisible(true);
-        test ts = new test();
-        ts.setVisible(true);
+//        test ts = new test();
+//        ts.setVisible(true);
     }
 }
