5228f97b941fbb584e78e018aeb0502d190705fb
rtfgvb74125 <105021056@live.asia.edu.tw>
2017-12-26 00:13:28 +0800

    20171226_2

--- src/GameControl.java
+++ src/GameControl.java
@@ -155,6 +155,7 @@ public class GameControl extends JFrame {
                         jlabBigM.setIcon(bigup);
                         gugubig();
                         brick();
+                        break;
 
                 }
             }
